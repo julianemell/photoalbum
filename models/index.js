3e5a4890b11fe3b141be62b3e10f070bdd850e75
Julia Nemell <jnemell@gmail.com>
2022-03-09 19:46:24 +0100

    added CLEARDB_DATABASEURL to knex

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const knex = require('knex')({
 	debug: true,
 	client: 'mysql',
-	connection: {
+	connection: process.env.CLEARDB_DATABASE_URL || {
 		host: process.env.DB_HOST || 'localhost',
 		port: process.env.DB_PORT || 3306,
 		charset: process.env.DB_CHARSET,
